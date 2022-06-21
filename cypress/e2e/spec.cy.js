6fb67d8100d462dd102082f65295d0577b7458be
shamilisrinivasan <shamili.srinivasan@globalsign.com>
2022-06-21 02:48:58 +0100

    Initial commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,76 @@
+/// <reference types="cypress" />
+
+
+import { randomgenaratefirstname,randomgenaratelastname,randomgenarateemailID } from '../commonutils/randomgenerator'
+import * as homepage from '../pages/createaccount'
+import * as searchpage from '../pages/searchpage'
+
+describe('Register in pearson application', () => {
+
+  beforeEach(()=>{
+    cy.viewport(900, 550)
+    cy.fixture('createaccount.json').then((inputdata)=>{
+      cy.visit(inputdata.pearson_url)
+    })
+    cy.wait(5000)
+    cy.get(homepage.btn_allow_all_cookie).click()
+  })
+
+  it('Create an account in pearson portal', () => {
+    cy.get('.userNav__button span').click()
+    cy.wait(1000)
+    cy.get(homepage.edt_firstname).scrollIntoView().should('be.visible')
+    cy.get(homepage.edt_firstname).type(randomgenaratefirstname())
+    cy.get(homepage.edt_lastname).type(randomgenaratelastname())
+    cy.get(homepage.edt_emailid).type(`${randomgenarateemailID(10)}@pearson.com`)
+    cy.get(homepage.edt_password).type('Password@1', { log: false })
+    cy.get(homepage.chk_accept_terms).click({force: true})
+    cy.get(homepage.chk_marketingopt).click()
+    cy.get(homepag.btn_create_account).click()
+    cy.get(homepag.btn_successful_login).should('be.visible')
+  })
+
+  it('Create an account in pearson portal with existing emailID', () => {
+    cy.get('.userNav__button span').click()
+    cy.wait(1000)
+    cy.get(homepage.edt_firstname).scrollIntoView().should('be.visible')
+    cy.get(homepage.edt_firstname).type(randomgenaratefirstname())
+    cy.get(homepage.edt_lastname).type(randomgenaratelastname())
+    cy.get(homepage.edt_emailid).then((inputdata)=>{
+      cy.get('#emailID').type(inputdata.existing_email)
+    })
+    cy.get(homepage.edt_password).type('Password@1', { log: false })
+    cy.get(homepage.chk_accept_terms).click({force: true})
+    cy.get(homepage.chk_marketingopt).click()
+    cy.get(homepage.btn_create_account).click()
+    cy.get(homepage.btn_successful_login).should('be.visible')
+    cy.get(homepage.lbl_error_msg).then(($errormessage)=>{
+      expect($errormessage.text()).to.contain('The username you have entered is already registered, please choose another one or reset your password to sign in.')
+    })
+  })
+
+  it('Search for titles', () => {
+    cy.get(searchpage.icn_search).click()
+    cy.get(searchpage.edt_search_text).type('Business & Economics{Enter}')
+    cy.wait(3000)
+    cy.get(searchpage.lbl_search_count).find('strong').should('have.text','1 - 25')
+    cy.get(searchpage.lbl_search_count).should('contain.text',' results')
+  }) 
+
+  it('Select first item from title searched ', () => {
+    cy.get(searchpage.icn_search).click()
+    cy.fixture('createaccount.json').then((inputdata)=>{
+      cy.get(searchpage.edt_search_text).type(`${inputdata.search_text}{Enter}`)
+    })
+    cy.wait(2000)
+    cy.get(searchpage.lst_firstitem_in_search).then(($productname)=>{
+      selectedproduct = $productname.text()
+      cy.get(searchpage.lst_firstitem_in_search).click()
+      cy.wait(2000)
+      cy.get(searchpage.lbl_page_heading).then(($producttitle)=>{
+        cy.log($producttitle.text())
+        expect($producttitle.text()).to.contain(selectedproduct)
+      })
+    })
+})
+})
---
+++ cypress/pages/createaccount.js
@@ -0,0 +1,13 @@
+export const btn_allow_all_cookie = ':nth-child(3) .optanon-alert-box-bg  .optanon-alert-box-button-container .optanon-button-allow .optanon-alert-box-button-middle .optanon-allow-all'
+export const btn_signin = '.userNav__button span'
+export const edt_firstname = '#firstName'
+export const edt_lastname = '#lastName'
+export const edt_emailid = '#emailID'
+export const edt_password = '#passwordID'
+
+export const chk_accept_terms = '[id="acceptTermsOfUse"]'
+export const chk_marketingopt = '#marketingOptInError .c-checkbox .c-checkbox__check'
+export const btn_create_account = '.button-container .button--primary'
+export const btn_successful_login = '.userNav__button'
+
+export const lbl_error_msg = '.c-alert__text'
